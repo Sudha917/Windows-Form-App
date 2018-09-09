c0a018586e4426e7bf5e02a5fdbff44d1038b724
sudha917 <sudha.avinash917.com>
2018-09-09 14:37:09 +0530

    Windows Form App

---
+++ WindowsFormsApP/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace WindowsFormsApP
+{
+    partial class LoginPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginPage));
+            this.label1 = new System.Windows.Forms.Label();
+            this.username = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pwd = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbltxt = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.errtxt = new System.Windows.Forms.Label();
+            this.ltxt = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(397, 159);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "User Name";
+            // 
+            // username
+            // 
+            this.username.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.username.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.username.Location = new System.Drawing.Point(539, 155);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(189, 29);
+            this.username.TabIndex = 1;
+            this.username.TextChanged += new System.EventHandler(this.username_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(411, 211);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // pwd
+            // 
+            this.pwd.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.pwd.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pwd.Location = new System.Drawing.Point(541, 208);
+            this.pwd.Name = "pwd";
+            this.pwd.PasswordChar = '*';
+            this.pwd.Size = new System.Drawing.Size(187, 29);
+            this.pwd.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Modern No. 20", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.label3.Location = new System.Drawing.Point(249, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(357, 29);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Windows Forms Application";
+            // 
+            // lbltxt
+            // 
+            this.lbltxt.AutoSize = true;
+            this.lbltxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltxt.ForeColor = System.Drawing.Color.Red;
+            this.lbltxt.Location = new System.Drawing.Point(585, 122);
+            this.lbltxt.Name = "lbltxt";
+            this.lbltxt.Size = new System.Drawing.Size(0, 20);
+            this.lbltxt.TabIndex = 7;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Image = global::WindowsFormsApP.Properties.Resources.ch1;
+            this.pictureBox1.ImageLocation = "";
+            this.pictureBox1.Location = new System.Drawing.Point(74, 110);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(297, 291);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.button2.BackgroundImage = global::WindowsFormsApP.Properties.Resources.Can;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(582, 273);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 43);
+            this.button2.TabIndex = 5;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.button1.BackgroundImage = global::WindowsFormsApP.Properties.Resources.lg;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(432, 272);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 44);
+            this.button1.TabIndex = 4;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // errtxt
+            // 
+            this.errtxt.AutoSize = true;
+            this.errtxt.ForeColor = System.Drawing.Color.Red;
+            this.errtxt.Location = new System.Drawing.Point(74, 74);
+            this.errtxt.Name = "errtxt";
+            this.errtxt.Size = new System.Drawing.Size(0, 13);
+            this.errtxt.TabIndex = 9;
+            // 
+            // ltxt
+            // 
+            this.ltxt.AutoSize = true;
+            this.ltxt.Location = new System.Drawing.Point(77, 74);
+            this.ltxt.Name = "ltxt";
+            this.ltxt.Size = new System.Drawing.Size(0, 13);
+            this.ltxt.TabIndex = 10;
+            this.ltxt.Visible = false;
+            // 
+            // LoginPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ltxt);
+            this.Controls.Add(this.errtxt);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lbltxt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pwd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "LoginPage";
+            this.Text = "LoginPage";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox pwd;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbltxt;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label errtxt;
+        private System.Windows.Forms.Label ltxt;
+    }
+}
+
---
+++ WindowsFormsApP/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApP
+{
+    public partial class LoginPage : Form
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            username.Focus();
+        }
+
+        private void username_TextChanged(object sender, EventArgs e)
+        {
+        //    String usr, pswd;
+        //    usr = username.Text;
+        //    pswd = pwd.Text;
+        //    if (String.IsNullOrEmpty(usr))
+        //    {
+        //        ltxt.Text = "Please Enter the UserName";
+        //       ltxt.Visible = true;
+        //   }
+         }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String usr, pswd;
+            usr = username.Text;
+            pswd = pwd.Text;
+
+            if (usr == "Admin" && pswd == "12345")
+            {
+                MessageBox.Show("Login Successful");
+                username.Text = null;
+                pwd.Text = null;
+                
+            }
+            else
+            {
+                MessageBox.Show("Login Failed Please try again");
+                username.Text = null;
+                pwd.Text = null;
+                username.Focus();
+            }
+        }
+
+
+    }
+}
